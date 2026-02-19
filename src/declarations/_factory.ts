cd59c0d3548f50166ce1b2af14b8b264741d3f0f
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-02-19 23:42:00 +0100

    Lint

--- src/declarations/_factory.ts
+++ src/declarations/_factory.ts
@@ -1,17 +1,17 @@
 import { idlFactory as idlFactoryBucket } from '$declarations/bucket/bucket.idl';
 import { idlFactory as idlFactoryDirectory } from '$declarations/directory/directory.idl';
 
-import { idlFactory as idlFactoryCertifiedDirectory } from '$declarations/directory/directory.certified.idl';
 import { idlFactory as idlFactoryCertifiedBucket } from '$declarations/bucket/bucket.certified.idl';
+import { idlFactory as idlFactoryCertifiedDirectory } from '$declarations/directory/directory.certified.idl';
 
-import type { _SERVICE as DirectoryService } from '$declarations/directory/directory';
 import type { _SERVICE as BucketService } from '$declarations/bucket/bucket';
+import type { _SERVICE as DirectoryService } from '$declarations/directory/directory';
 
 export {
-	idlFactoryCertifiedBucket,
 	idlFactoryBucket,
-	type BucketService,
+	idlFactoryCertifiedBucket,
 	idlFactoryCertifiedDirectory,
 	idlFactoryDirectory,
+	type BucketService,
 	type DirectoryService
 };
