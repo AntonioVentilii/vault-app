7912412b402a7c5213f77f9fdc29fe3b4a68b9e1
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-02-20 00:17:16 +0100

    ICRC API

---
+++ src/lib/constants/app.constants.ts
@@ -0,0 +1,7 @@
+export const MODE = 'local';
+export const LOCAL = MODE === 'local';
+
+export const REPLICA_HOST = LOCAL ? 'http://localhost:5987/' : 'https://icp-api.io';
+
+// eslint-disable-next-line no-restricted-syntax -- This is the definition
+export const ZERO = 0n;
