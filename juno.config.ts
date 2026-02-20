9ddc9378ba9d69fb81e589f42b24196828ace2a0
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-02-20 06:52:19 +0100

    Juno Deploy

--- juno.config.ts
+++ juno.config.ts
@@ -4,9 +4,12 @@ export default defineConfig({
 	satellite: {
 		ids: {
 			development: '<DEV_SATELLITE_ID>',
-			production: '<PROD_SATELLITE_ID>'
+			production: 'z6kq3-iqaaa-aaaal-asxhq-cai'
 		},
 		source: 'build',
 		predeploy: ['npm run build']
+	},
+	orbiter: {
+		id: 'gfpjj-5iaaa-aaaal-amr4a-cai'
 	}
 });
